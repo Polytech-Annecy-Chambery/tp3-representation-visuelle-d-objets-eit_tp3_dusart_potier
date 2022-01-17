dacd1e3dcfefdc8c02c13341906d5a8662bb3aa8
EIT-DusartSiméon-TP1 <91719985+DusartS@users.noreply.github.com>
2022-01-17 17:22:45 +0100

    Update Opening.py

--- src/Opening.py
+++ src/Opening.py
@@ -46,14 +46,43 @@ class Opening:
 
     # Defines the vertices and faces        
     def generate(self):
-        self.vertices = [ 
-                # Définir ici les sommets
+        self.vertices = [ # Définir ici les sommets
+                [self.parameters['position']],
+                [self.parameters['position'][0]+self.parameters['width'],self.parameters['position'][1],
+                 self.parameters['position'][2]],
+                [self.parameters['position'][0]+self.parameters['width'],self.parameters['position'][1],
+                 self.parameters['position'][2]+self.parameters['height']],
+                [self.parameters['position'][0],self.parameters['position'][1],
+                 self.parameters['position'][2]+self.parameters['height']],
+                [self.parameters['position'][0],self.parameters['position'][1]+self.parameters['thickness'],
+                 self.parameters['position'][2]+self.parameters['height']],
+                [self.parameters['position'][0]+self.parameters['width'],
+                 self.parameters['position'][1]+self.parameters['thickness'],
+                 self.parameters['position'][2]+self.parameters['height']],
+                [self.parameters['position'][0]+self.parameters['width'],
+                 self.parameters['position'][1]+self.parameters['thickness'],self.parameters['position'][2]],
+                [self.parameters['position'][0],self.parameters['position'][1]+self.parameters['thickness'],
+                 self.parameters['position'][2]]
                 ]
-        self.faces = [
-                # définir ici les faces
+        self.faces = [  # définir ici les faces
+                [1,6,5,2],
+                [7,0,3,4],
+                [0,1,6,7],
+                [3,3,5,4]
                 ]   
         
     # Draws the faces                
     def draw(self):        
         # A compléter en remplaçant pass par votre code
-        pass
+        gl.glPushMatrix()
+        gl.glTranslatef(self.parameters['position'][0],self.parameters['position'][1],self.parameters['position'][2])
+        gl.glPolygonMode(gl.GL_FRONT_AND_BACK,gl.GL_FILL)
+        gl.glBegin(gl.GL_QUADS)
+        gl.glColor3fv([0.5,0.5,0.5])
+        for i in self.faces :
+            gl.glVertex3fv(self.vertices[i[0]])
+            gl.glVertex3fv(self.vertices[i[1]])
+            gl.glVertex3fv(self.vertices[i[2]])
+            gl.glVertex3fv(self.vertices[i[3]])
+        gl.glEnd()
+        gl.glPopMatrix()
